d1a0af92db80d8009b1c98e33be90bef03e06227
Andreina <andreina.ferreira@academico.ifpb.edu.br>
2025-08-15 13:51:42 -0300

    Adiciona classes de modelo do banco de dados

---
+++ models.py
@@ -0,0 +1,14 @@
+from database import db
+
+class Categoria(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    descricao = db.Column(db.String(200))
+    produtos = db.relationship('Produto', backref='categoria', lazy=True)
+
+class Produto(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    preco = db.Column(db.Float, nullable=False)
+    estoque = db.Column(db.Integer, nullable=False)
+    categoria_id = db.Column(db.Integer, db.ForeignKey('categoria.id'), nullable=False)
